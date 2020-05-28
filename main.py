8d9227633918b1080bbe8aa7183bdb2dd3deab9c
SkyHopper21 <chaitanykulkarni21@gmail.com>
2020-05-28 20:21:25 +0530

    Seprate Grid

--- main.py
+++ main.py
@@ -7,97 +7,20 @@ except:
     import install_req  # install packages
 import threading
 from Heap import Heap
-from Node import Node
+import Grid
 import AStar
 
 pygame.init()
 screen = pygame.display.set_mode((800, 800))
 
-cols = 50
-rows = 50
-grid = [0 for i in range(cols)]
 openSet = Heap()
 closedSet = []
 path = []
-pink = (255, 125, 125)
-red = (150, 50, 50)
-green = (0, 255, 0)
-blue = (0, 0, 255)
 grey = (220, 220, 220)
-w = 800 / cols
-h = 800 / rows
-cameFrom = []
-start = -1
-end = -1
-lastDoing = -1
+pink = (255, 125, 125)
 
-Node.Init(pygame, screen, cols, rows)
 screen.fill(grey)
-
-
-for i in range(cols):
-    grid[i] = [0 for i in range(rows)]
-
-# Create Nodes
-for i in range(cols):
-    for j in range(rows):
-        grid[i][j] = Node(i, j)
-
-# showing all nodes
-for i in range(cols):
-    for j in range(rows):
-        grid[i][j].Show((255, 255, 255), 1)
-
-# Creating border around
-for i in range(0, rows):
-    grid[0][i].Show(grey, 0)
-    grid[0][i].obstacle = True
-    grid[cols-1][i].obstacle = True
-    grid[cols-1][i].Show(grey, 0)
-    grid[i][0].Show(grey, 0)
-    grid[i][0].obstacle = True
-    grid[i][rows-1].Show(grey, 0)
-    grid[i][rows-1].obstacle = True
-
-
-def GetNodeFromPos(pos):
-    x = pos[0]
-    y = pos[1]
-    g1 = x // (800 // cols)
-    g2 = y // (800 // rows)
-
-    return grid[g1][g2]
-
-
-def SetTarget(pos):
-    global start, end
-    node = GetNodeFromPos(pos)
-    if node == start:
-        node.Show(grey, 0)
-        node.Show((255, 255, 255), 1)
-        start = -1
-        return
-    elif start == -1:
-        start = node
-    else:
-        end = node
-    node.Show(pink, 0)
-
-
-def SetObstacle(pos):
-    global lastDoing
-    node = GetNodeFromPos(pos)
-    if node != start and node != end:
-        if lastDoing == -1:
-            lastDoing = node.obstacle
-        if lastDoing != node.obstacle:
-            return
-        node.obstacle = not node.obstacle
-        if node.obstacle:
-            node.Show((0, 0, 0), 0)  # setting black
-        else:
-            node.Show(grey, 0)
-            node.Show((255, 255, 255), 1)
+Grid.Init(pygame, screen)
 
 
 loop = True
@@ -107,25 +30,23 @@ while loop:
             pygame.quit()
             sys.exit(1)
         if event.type == pygame.MOUSEBUTTONUP:
-            lastDoing = -1
+            Grid.lastDoing = -1
         if pygame.mouse.get_pressed()[0]:
-            if start == -1 or end == -1:
-                SetTarget(pygame.mouse.get_pos())
+            if Grid.start == -1 or Grid.end == -1:
+                Grid.SetTarget(pygame.mouse.get_pos())
             else:
-                SetObstacle(pygame.mouse.get_pos())
+                Grid.SetObstacle(pygame.mouse.get_pos())
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 loop = False
                 break
 
-for i in range(cols):
-    for j in range(rows):
-        grid[i][j].addNeighbours(grid)
-start.parent = start
-openSet.push(start)
+Grid.SetNeighbours()
+Grid.start.parent = Grid.start
+openSet.push(Grid.start)
 
 
-while AStar.Solve(start, end, openSet, closedSet, path, steps=True):
+while AStar.Solve(Grid.start, Grid.end, openSet, closedSet, path, steps=True):
     if pygame.event.poll().type == pygame.QUIT:
         pygame.quit()
         sys.exit(0)
@@ -156,40 +77,14 @@ def Clear(cPath=False):
         path = []
     openSet.clear()
     closedSet = []
-    start.gCost = 0
-    start.hCost = 0
-    start.fCost = 0
-    start.Show(pink, 0)
-    end.Show(pink, 0)
-
-
-def MoveTargets(selected, moveTo):
-    global start, end
-    if moveTo.obstacle or moveTo == selected:
-        return False
-    if selected == start:
-        if moveTo == end:
-            return False
-
-        selected.Show(grey, 0)
-        selected.Show((255, 255, 255), 1)
-        start = moveTo
-
-    elif selected == end:
-        if moveTo == start:
-            return False
-
-        selected.Show(grey, 0)
-        selected.Show((255, 255, 255), 1)
-        end = moveTo
-    start.Show(pink, 0)
-    end.Show(pink, 0)
-    return True
+    Grid.start.gCost = 0
+    Grid.start.hCost = 0
+    Grid.start.fCost = 0
+    Grid.start.Show(pink, 0)
+    Grid.end.Show(pink, 0)
 
 
 selected = None
-prevStart = start
-prevEnd = end
 Clear()
 while True:
     for event in pygame.event.get():
@@ -197,20 +92,18 @@ while True:
             pygame.quit()
             sys.exit(1)
         if event.type == pygame.MOUSEBUTTONDOWN:
-            selected = GetNodeFromPos(pygame.mouse.get_pos())
-            if selected != start and selected != end:
-                selected = None
+            selected = Grid.GetNodeFromPos(
+                pygame.mouse.get_pos(), onlyTargets=True)
         if pygame.mouse.get_pressed()[0]:
             if selected != None:
-                moveTo = GetNodeFromPos(pygame.mouse.get_pos())
-                if MoveTargets(selected, moveTo):
+                moveTo = Grid.GetNodeFromPos(pygame.mouse.get_pos())
+                if Grid.MoveTargets(selected, moveTo):
                     selected = moveTo
         if event.type == pygame.MOUSEBUTTONUP:
             selected = None
-    if prevStart != start or prevEnd != end and selected == None:
+    if Grid.isChanged and selected == None:
+        Grid.isChanged = False
         Clear(True)
-        prevStart = start
-        prevEnd = end
-        start.parent = start
-        openSet.push(start)
-        AStar.Solve(start, end, openSet, closedSet, path)
+        Grid.start.parent = Grid.start
+        openSet.push(Grid.start)
+        AStar.Solve(Grid.start, Grid.end, openSet, closedSet, path)
